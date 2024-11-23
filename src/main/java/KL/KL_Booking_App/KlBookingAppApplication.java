4127f737d5c91d6ec0f914f95827756f561b78ef
Vinh <vinhhoangvh11@gmail.com>
2024-11-23 20:37:19 +0700

    first initial

---
+++ src/main/java/KL/KL_Booking_App/KlBookingAppApplication.java
@@ -0,0 +1,13 @@
+package KL.KL_Booking_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlBookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KL/KL_Booking_App/KlBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package KL.KL_Booking_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
